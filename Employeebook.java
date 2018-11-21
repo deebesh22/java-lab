a6a226ba2f963c66483dfb35e0c3be4f5bcf90be
deebesh22 <44742800+deebesh22@users.noreply.github.com>
2018-11-21 20:53:52 +0530

    Add files via upload

---
+++ Employeebook.java
@@ -0,0 +1,49 @@
+package employee;
+
+import java.util.scanner;
+
+public class Employeebook
+
+{
+  
+   public void Employeedetails();
+
+    {
+      
+       int employeeid;
+  
+       String employeename;
+
+       String department;
+
+       int doj;
+ 
+       char marstat;
+
+       int dob;
+  
+       System.out.println("1234");
+ 
+       System.out.println("ram");
+  
+       System.out.println("infotech");
+  
+       System.out.println("12092018");
+   
+       System.out.println("good");
+    
+       System.out.println(12051988);
+
+    }
+
+    public static void main(String args[])
+ 
+   {
+   
+     Employeebook emp=new Employeebook();
+  
+      emp.Employeedetails();
+   
+ }
+
+}
