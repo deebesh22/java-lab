7b632b578ba1a4e8094e34f203a0e044941651d0
deebesh22 <44742800+deebesh22@users.noreply.github.com>
2018-11-21 21:24:45 +0530

    Add files via upload

---
+++ Employeedata.java
@@ -0,0 +1,42 @@
+package alchem;
+
+import java.util.Scanner;
+
+public class Employeedetails
+
+{
+    
+ public void showdetails()
+ 
+   {
+    
+    int option;
+
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("...menu...");
+   
+    System.out.println("enter data");
+    
+    System.out.println("update data");
+ 
+    System.out.println("display data");
+  
+    System.out.println("exit");
+     
+    System.out.println("enter the option :");
+  
+    option=sc.nextint();
+ 
+   }
+    
+     public static void main(String args[]);
+  
+  {
+  
+     Employeedetails emp=new employeedetails();
+   
+     emp.showdetails();
+   
+  }
+ }
