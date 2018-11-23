6fc5218dfc8624108651ab720564878910550bfa
deebesh22 <44742800+deebesh22@users.noreply.github.com>
2018-11-23 21:11:05 +0530

    Add files via upload

---
+++ mango.java
@@ -0,0 +1,24 @@
+public class Mangovariety
+
+{
+   
+ enum Mango{carrie(10),fairchild(9),haden(12)};
+
+ private int price;
+
+ mango(int p)
+  
+ {
+    
+  price=p;
+   
+ }
+   
+ int getprice()
+ 
+ {
+        
+  return price;
+   
+ }
+}
