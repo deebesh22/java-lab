c31254dec330f365505a98c8f7cc00168c70c19a
deebesh22 <44742800+deebesh22@users.noreply.github.com>
2018-11-23 21:19:13 +0530

    Add files via upload

---
+++ sampleclass.java
@@ -0,0 +1,25 @@
+public class Sampleclass
+
+{
+
+    public int counter=1;
+
+    void display()
+
+    {
+ 
+      System.out.println("counter");
+ 
+    }
+
+    public static void main(String args[])
+ 
+   {
+ 
+       Sampleclass samp=new Sampleclass();
+ 
+       samp.display();
+ 
+   }
+
+}
