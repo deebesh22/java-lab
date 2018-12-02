c5e61b9655ac1193a5f06d8ebb5acde3259318ad
deebesh22 <44742800+deebesh22@users.noreply.github.com>
2018-12-02 15:01:34 +0530

    Add files via upload

---
+++ reservation.java
@@ -0,0 +1,18 @@
+class Reservation
+{
+ int ticketid;
+ public void showticket()
+ {
+  System.out.println("243546");
+ }
+ Reservation()
+ {
+  this.ticketid=ticketid;
+ }
+ public static void main(String args[])
+ {
+  Reservation rs=new Reservation();
+  rs.showticket();
+ }
+}
+ 
